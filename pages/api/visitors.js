7928606ec5a5d1176a094ef22c04a707bc76a7d5
Nick Mackenzie <mackenzie.nick@outlook.com>
2022-02-01 22:29:09 -0500

    changes

--- pages/api/visitors.js
+++ pages/api/visitors.js
@@ -5,32 +5,44 @@ import { format } from 'date-fns'
 export default async function visitor(req, res) {
     const client = await clientPromise;
     const db = client.db("portfolio");
-
+    let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
     switch (req.method) {
         case "POST":
-            console.log(req.body.visitorData)
-            let currentDate = format(new Date(), 'MMMM/dd/yyyy')
-            let currentTime = format(new Date(), 'h:mmbbbb')
-            let visitorObject = req.body.visitorData
-            visitorObject.currentDate = currentDate;
-            visitorObject.currentTime = currentTime;
-            let newVisitor = await db.collection("visitors").insertOne(visitorObject);
-            res.json({ status: 200, data: newVisitor });
-            // axios.post(`https://ipinfo.io?token=${process.env.VISITORAPI}`)
-            //     .then(async function (response) {
-            //         let currentDate = format(new Date(), 'MMMM/dd/yyyy')
-            //         let currentTime = format(new Date(), 'h:mmbbbb')
-            //         let visitorObject = response.data
-            //         visitorObject.currentDate = currentDate;
-            //         visitorObject.currentTime = currentTime;
-            //         let newVisitor = await db.collection("visitors").insertOne(visitorObject);
-            //         res.json({ status: 200, data: newVisitor });
-            //     })
-            //     .catch(function (error) {
-            //         res.json({ status: 200, data: error });
+            // console.log(req.body.visitorData)
+            // let currentDate = format(new Date(), 'MMMM/dd/yyyy')
+            // let currentTime = format(new Date(), 'h:mmbbbb')
+            // let visitorObject = req.body.visitorData
+            // visitorObject.currentDate = currentDate;
+            // visitorObject.currentTime = currentTime;
+            // let newVisitor = await db.collection("visitors").insertOne(visitorObject);
+            let ip;
+            let country;
+            let city;
+            let regionName
+            await axios.get('https://api.ipify.org/?format=json').then(async function (response) {
+                ip = response.data.ip
+            })
+            await axios.get(`http://ip-api.com/json/${ip}`).then(async function (response) {
+                country = response.data.country
+                city = response.data.city
+                regionName = response.data.regionName
+            })
 
 
-            //     });
+            axios.post(`https://push.techulus.com/api/v1/notify/${process.env.PUSH_API}`,
+                {     //body
+                    "title": `${timezone}`,
+                    "body": `${city}, ${country} in ${regionName}`
+                })
+                .then(async function (response) {
+                    console.log(response)
+                    res.json({ status: 200, data: newVisitor });
+                })
+                .catch(function (error) {
+                    res.json({ status: 200, data: error });
+
+
+                });
             break;
         case "GET":
             const posts = await db.collection("visitors").find({}).toArray();
